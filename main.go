2a560df7abe23be721a6e24a9035800117f3601f
hrushikesh <hrushi2002j@gmail.com>
2024-05-09 03:20:04 +0530

    feat: remove mouse scroll

--- main.go
+++ main.go
@@ -60,26 +60,13 @@ func main() {
 	}
 }
 
-// You can wire any Bubble Tea model up to the middleware with a function that
-// handles the incoming ssh.Session. Here we just grab the terminal info and
-// pass it to the new model. You can also return tea.ProgramOptions (such as
-// tea.WithAltScreen) on a session by session basis.
-func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
-	// This should never fail, as we are using the activeterm middleware.
-	pty, _, _ := s.Pty()
 
-	// When running a Bubble Tea app over SSH, you shouldn't use the default
-	// lipgloss.NewStyle function.
-	// That function will use the color profile from the os.Stdin, which is the
-	// server, not the client.
-	// We provide a MakeRenderer function in the bubbletea middleware package,
-	// so you can easily get the correct renderer for the current session, and
-	// use it to create the styles.
-	// The recommended way to use these styles is to then pass them down to
-	// your Bubble Tea model.
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	pty, _, _ := s.Pty()
 	renderer := bubbletea.MakeRenderer(s)
 
 	m := cv.NewModel(pty.Window.Width, pty.Window.Height, renderer)
 
-	return m, []tea.ProgramOption{tea.WithAltScreen(), tea.WithMouseAllMotion()}
+	// mouse scroll: tea.WithMouseAllMotion()
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
 }
